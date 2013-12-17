788384d6b9471af31e0f6b8f5dd7c0c6e307d877
Michael Dominick <michael@fingertiptech.com>
2013-12-17 14:36:41 -0500

    Initial Commit

---
+++ MDNetworking/MDNetworking.h
@@ -0,0 +1,13 @@
+//
+//  MDNetworking.h
+//  MDNetworking
+//
+//  Created by Michael on 12/17/13.
+//  Copyright (c) 2013 fingertiptech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDNetworking : NSObject
+
+@end
---
+++ MDNetworking/MDNetworking.m
@@ -0,0 +1,13 @@
+//
+//  MDNetworking.m
+//  MDNetworking
+//
+//  Created by Michael on 12/17/13.
+//  Copyright (c) 2013 fingertiptech. All rights reserved.
+//
+
+#import "MDNetworking.h"
+
+@implementation MDNetworking
+
+@end
