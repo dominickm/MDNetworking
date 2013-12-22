02f8f5d92088f29c42c6db7848f7ada31cbe736d
Michael Dominick <michael@fingertiptech.com>
2013-12-22 18:46:45 -0500

    Added a static contructor for MDURLRequest that accomodates HTTP Auth

--- MDNetworking/MDURLRequest.h
+++ MDNetworking/MDURLRequest.h
@@ -12,6 +12,8 @@
 
 + (MDURLRequest *)requestWithURL:(id)url andHTTPType:(NSString *)httpType;
 
++ (MDURLRequest *)requestWithURL:(id)url HTTPType:(NSString *)httpType Username:(NSString *)username andPassword:(NSString *)password;
+
 - (void)startAsynchWithSuccess:(void (^)(NSURLResponse* response, NSData* data, NSError* connectionError))completition;
 
 @end
--- MDNetworking/MDURLRequest.m
+++ MDNetworking/MDURLRequest.m
@@ -34,10 +34,37 @@
     return req;
 }
 
+// Init with HTTP Auth
+
++ (MDURLRequest *)requestWithURL:(id)url HTTPType:(NSString *)httpType Username:(NSString *)username andPassword:(NSString *)password
+{
+    MDURLRequest* req = [[MDURLRequest alloc] init];
+    
+    if ([url isKindOfClass:[MDURLRequest class]])
+    {
+        req = [MDURLRequest requestWithURL:url];
+    }
+    else if ([url isKindOfClass:[NSString class]])
+    {
+        req = [MDURLRequest requestWithURL:[MDURL URLWithString:url]];
+    }
+    
+    if (httpType)
+    {
+        req.HTTPMethod = httpType;
+    }
+    
+    NSString* basicAuthString = [NSString stringWithFormat:@"%@:%@", username, password];
+    [req setValue:basicAuthString forHTTPHeaderField:@"Authorization"];
+    
+    return req;
+}
+
 - (void)startAsynchWithSuccess:(void (^)(NSURLResponse*, NSData*, NSError* ))completition
 {
     [NSURLConnection sendAsynchronousRequest:self queue:[NSOperationQueue mainQueue] completionHandler:completition];
 }
+
 
 @end
 
